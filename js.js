cbb5b3aa1bd4cf20bc161f30d21829c372e03d54
gbrl_hrny <gabrielhenryolliv@gmail.com>
2024-04-04 20:39:59 -0300

    implementando codigos

--- js.js
+++ js.js
@@ -1,12 +1,18 @@
-    // Selecionando o botão e o texto
-    const toggleButton = document.getElementById('toggleButton');
-    const text = document.getElementById('text');
-
-    // Adicionando um evento de clique ao botão
-    toggleButton.addEventListener('click', function() {
-        // Gerando uma cor aleatória para o texto
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        
-        // Alterando a cor do texto
-        text.style.color = randomColor;
-    });
+function mostrarDivs() {
+    var radios = document.getElementsByName('grupo');
+    var divs = document.getElementsByClassName('divs');
+    
+    for (var i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        for (var j = 0; j < divs.length; j++) {
+          divs[j].style.display = 'none';
+        }
+        var valor = radios[i].value;
+        var divsSelecionadas = document.getElementsByClassName(valor);
+        for (var k = 0; k < divsSelecionadas.length; k++) {
+          divsSelecionadas[k].style.display = 'inline-block';
+        }
+        break;
+      }
+    }
+}
